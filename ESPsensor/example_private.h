d8094e8a01c0ab5f313c944dbbfabd486ba94ad1
cctweaker <>
2020-06-05 08:30:45 +0300

    added info

--- ESPsensor/example_private.h
+++ ESPsensor/example_private.h
@@ -22,12 +22,13 @@
 uint8_t gmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 // using a custom mac will allow you to change the ESP on the gateway without changing the programming on all sensors and buttons
 // hardware mac address assigned to the ESP is unchanged, just not used in this case
+// make sure to use the same gateway mac address in all projects!
 //
 uint8_t smac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE}; // sensors
-uint8_t bmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF}; // buttons/switches
-uint8_t dmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xF0}; // door switches
-uint8_t wmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xF1}; // window switches
-uint8_t mmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xF2}; // movement detectors, light barriers
+// uint8_t bmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF}; // buttons/switches
+// uint8_t dmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xF0}; // door switches
+// uint8_t wmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xF1}; // window switches
+// uint8_t mmac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xF2}; // movement detectors, light barriers
 // ESP-GW can load up to 6 mac addresses following its own mac
 // ESP-Now protocol allows up to 6 mac addresses in encrypted mode
 /////////////////////////////////////////////////
@@ -45,9 +46,10 @@ uint8_t key[16] = {0xFE, 0xED, 0xFE, 0xED, 0xFE, 0xED, 0xFE, 0xED, 0xFE, 0xED, 0
 // TIP will be sent in json message
 /////////////////////////////////////////////////
 #define TIP "sensor" // sensor
-// #define TIP "switch" // light switch
-// #define TIP "door"   // door switch
-// #define TIP "window" // window switch
+// #define TIP "switch"   // light switch
+// #define TIP "door"     // door switch
+// #define TIP "window"   // window switch
+// #define TIP "movement" // presence detectors, light barriers, etc
 /////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
